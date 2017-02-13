f51a260e7def41d0c58545c6bc16d1e8a4f6c0b6
Liam Nickell <liam.nickell25@gmail.com>
2017-02-12 19:39:42 -0700

    did chapter 0 exercises

---
+++ 00/HelloWorld.cpp
@@ -0,0 +1,7 @@
+// Exercise 0-0
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
---
+++ 00/PrintingCode.cpp
@@ -0,0 +1,13 @@
+// Exercise 0-4
+#include <iostream>
+
+int main() {
+	std::cout << "// a small C++ program" << std::endl;
+	std::cout << "#include <iostream>\n" << std::endl;
+	std::cout << "int main() {" << std::endl;
+	std::cout << "\tstd::cout << \"Hello, World!\" << std::endl;" << std::endl;
+	std::cout << "\treturn 0;" << std::endl;
+	std::cout << "}" << std::endl;
+
+	return 0;
+}
